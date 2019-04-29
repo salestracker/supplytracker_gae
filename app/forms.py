9073d2ee9139060cf001254c0ac7913b5abbb492
Akul Mathur <akulm@google.com>
2019-04-29 19:40:54 +0530

    restored original app.db. fixed super() to backward compatible py2

--- app/forms.py
+++ app/forms.py
@@ -33,9 +33,9 @@ class IdForm(Form):
   id = IntegerField(label='id',
                     validators=[InputRequired()],
                     widget=HiddenInput())
-  
+
   def __init__(self, id, url, **kwargs):
-    super().__init__(**kwargs)
+    super(type(self), self).__init__(**kwargs)
     self.id.data = id
     self.url = url
 
@@ -51,11 +51,11 @@ class ItemTypeForm(Form):
                            validators=[InputRequired(), Length(min=1, max=models.ITEM_TYPE_LENGTH)])
   submit_button = SubmitField(label='Save item type')
   check_exist = True
-  
+
   def validate(self, **kwargs):
-    if not super().validate():
+    if not super(type(self), self).validate():
       return False
-    
+
     item_name = models.ItemType.format_item_type(self.name_field.data)
     # check if the item exists
     if not self.check_exist or not models.ItemType.check_exist(item_name):
@@ -71,11 +71,11 @@ class CourierForm(Form):
                            validators=[InputRequired(), Length(min=1, max=models.ITEM_TYPE_LENGTH)])
   submit_button = SubmitField(label='Save courier')
   check_exist = True
-  
+
   def validate(self, **kwargs):
-    if not super().validate():
+    if not super(type(self), self).validate():
       return False
-    
+
     # check if the item exists
     if not self.check_exist or not models.Courier.check_exist(self.name_field.data):
       return True
@@ -89,11 +89,11 @@ class TransactionMediumForm(Form):
   name_field = StringField(label='Transaction Medium',
                            validators=[InputRequired(), Length(min=1, max=models.ITEM_TYPE_LENGTH)])
   submit_button = SubmitField(label='Save transaction medium')
-  
+
   def validate(self, **kwargs):
-    if not super().validate():
+    if not super(type(self), self).validate():
       return False
-    
+
     # check if the item exists
     if not models.TransactionMedium.check_exist(self.name_field.data):
       return True
@@ -140,22 +140,22 @@ class ItemPurchaseForm(wtforms.Form):
   item_type = SelectField(label='Item Type: ',
                           coerce=int,
                           validators=[InputRequired()])
-  
+
   quantity = IntegerField(label='Item Quantity',
                           validators=[InputRequired()])
-  
+
   def validate(self, *args, **kwargs):
-    if not super().validate():
-      self.purchase_price.errors.append(super().errors)
+    if not super(type(self), self).validate():
+      self.purchase_price.errors.append(super(type(self), self).errors)
       return False
-    
+
     if self.quantity.data < 0:
       self.quantity.errors.append('Quantity cannot be negative')
-    
+
     return True
-  
+
   def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
+    super(type(self), self).__init__(*args, **kwargs)
     self.item_type.choices = get_item_type_list()
 
 
@@ -166,7 +166,7 @@ class PurchaseTransactionForm(Form):
                     validators=[InputRequired()])
   dd = IntegerField(label='Transaction Day',
                     validators=[InputRequired()])
-  
+
   HH = IntegerField(label='Transaction Hour',
                     validators=[InputRequired()])
   mm = IntegerField(label='Transaction Minute',
@@ -177,22 +177,22 @@ class PurchaseTransactionForm(Form):
   supplier_id = SelectField(label='Supplier: ',
                             coerce=int,
                             validators=[InputRequired()])
-  
+
   notes = StringField(label='Notes',
                       validators=[Length(max=models.NOTES_LENGTH)],
                       widget=TextArea())
-  
+
   transaction_items = FieldList(FormField(ItemPurchaseForm),
                                 label='Purchased items',
                                 min_entries=1)
   submit_button = SubmitField(label='Save purchase transaction')
-  
+
   def validate(self, **kwargs):
-    
-    if not super().validate():
-      self.yyyy.errors += (super().errors, 'super not validated')
+
+    if not super(type(self), self).validate():
+      self.yyyy.errors += (super(type(self), self).errors, 'super not validated')
       return False
-    
+
     # check if the date is valid, also this has included the leap year factor
     try:
       dt = datetime(self.yyyy.data, self.MM.data, self.dd.data,
@@ -200,17 +200,17 @@ class PurchaseTransactionForm(Form):
     except ValueError as ve:
       self.yyyy.errors += (ve,)
       return False
-    
+
     if dt > datetime.now():
       self.yyyy.errors += ('Date time cannot be in the future',
                            'current date: {}'.format(datetime.now()),
                            'input date: {}'.format(dt))
       return False
-    
+
     return True
-  
+
   def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
+    super(type(self), self).__init__(*args, **kwargs)
     self.supplier_id.choices = get_supplier_list()
 
 
@@ -223,17 +223,17 @@ def get_unsold_items_dict(name_key, stock_key, sold_key):
                                   func.count(models.Item.id), models.Item.sale_transaction_id) \
     .join(models.ItemType) \
     .group_by(models.Item.item_type_id, models.Item.sale_transaction_id)
-  
+
   # flatten the dict to merge everything based on id, name, stock, sold
   flattened_sale_dict = {}
-  
+
   for item in unsold_items:
     if item[0] not in flattened_sale_dict:
       # format [name, qty_available, qty_sold]
       flattened_sale_dict[item[0]] = {name_key: item[1],
                                       stock_key: 0,
                                       sold_key: 0}
-    
+
     # item has been sold because
     # sale_transaction_id exists
     # change the total item sold
@@ -241,7 +241,7 @@ def get_unsold_items_dict(name_key, stock_key, sold_key):
       flattened_sale_dict[item[0]][sold_key] = item[2]
     else:
       flattened_sale_dict[item[0]][stock_key] = item[2]
-  
+
   return flattened_sale_dict
 
 
@@ -267,28 +267,28 @@ def get_medium_list():
 class ItemSaleForm(wtforms.Form):
   stock_separator = ' - stock: '
   sold_separator = ' - sold: '
-  
+
   sale_price = IntegerField(label='Sale Price (each): ',
                             validators=[InputRequired()])
-  
+
   # declare choice later on because wtform behaving weird not updating choices after database update
   item_stock = SelectField(label='Item Type: ',
                            coerce=int,
                            validators=[DataRequired()])
   quantity = IntegerField(label='Item Quantity',
                           validators=[InputRequired()])
-  
+
   def validate(self, *args, **kwargs):
-    if not super().validate():
-      self.sale_price.errors += (super().errors,)
+    if not super(type(self), self).validate():
+      self.sale_price.errors += (super(type(self), self).errors,)
       return False
-    
+
     if self.quantity.data < 0:
       self.quantity.errors.append('Quantity cannot be negative')
-    
+
     # TODO include the edit sale transaction stock
     # and put back later on check qty total
-    
+
     # get maximum quantity for selected item_type
     # max_qty = int(self.unsold_items_dict[self.item_stock.data]['stock'])
     #
@@ -297,11 +297,11 @@ class ItemSaleForm(wtforms.Form):
     #         ("Quantity exceeds the item's stock for {}"\
     #         .format(self.unsold_items_dict[self.item_stock.data]['name']),)
     #     return False
-    
+
     return True
-  
+
   def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
+    super(type(self), self).__init__(*args, **kwargs)
     self.unsold_items_dict = get_unsold_items_dict('name', 'stock', 'sold')
     # convert to dict
     # cannot use dict comprehension because the scope inside dict comprehension
@@ -309,7 +309,7 @@ class ItemSaleForm(wtforms.Form):
     choices = [(k, '{}{}{}{}{}'.format(v['name'], self.stock_separator,
                                        v['stock'], self.sold_separator,
                                        v['sold'])) for k, v in self.unsold_items_dict.items()]
-    
+
     self.item_stock.choices = choices
 
 
@@ -320,14 +320,14 @@ class SaleTransactionForm(Form):
                     validators=[InputRequired()])
   dd = IntegerField(label='Transaction Day',
                     validators=[InputRequired()])
-  
+
   HH = IntegerField(label='Transaction Hour',
                     validators=[InputRequired()])
   mm = IntegerField(label='Transaction Minute',
                     validators=[InputRequired()])
   ss = IntegerField(label='Transaction Second',
                     default=0, validators=[InputRequired()])
-  
+
   delivery_fee = IntegerField(label='Delivery Fee', validators=[Optional()])
   customer_id = SelectField(label='Customer',
                             coerce=int,
@@ -335,23 +335,23 @@ class SaleTransactionForm(Form):
   courier_id = SelectField(label='Courier', coerce=int, validators=[Optional()])
   transaction_medium_id = SelectField(label='Transaction Medium',
                                       coerce=int, validators=[Optional()])
-  
+
   notes = StringField(label='Notes',
                       validators=[Length(max=models.NOTES_LENGTH)],
                       widget=TextArea())
-  
+
   transaction_items = FieldList(FormField(ItemSaleForm),
                                 label='Sale items',
                                 min_entries=1)
   submit_button = SubmitField(label='Save Sale Transaction')
-  
+
   transaction_items_choices_dict = {}
-  
+
   def validate(self, **kwargs):
-    if not super().validate():
-      self.yyyy.errors += (super().errors, 'super not validated')
+    if not super(type(self), self).validate():
+      self.yyyy.errors += (super(type(self), self).errors, 'super not validated')
       return False
-    
+
     # check if the date is valid, also this include the leap year factor
     try:
       dt = datetime(self.yyyy.data, self.MM.data, self.dd.data,
@@ -359,20 +359,20 @@ class SaleTransactionForm(Form):
     except ValueError as ve:
       self.yyyy.errors += (ve,)
       return False
-    
+
     if dt > datetime.now():
       self.yyyy.errors += ('Date time cannot be in the future',
                            'current date: {}'.format(datetime.now()),
                            'input date: {}'.format(dt))
       return False
-    
+
     int_field_convert_to_none(self.courier_id)
     int_field_convert_to_none(self.transaction_medium_id)
-    
+
     return True
-  
+
   def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
+    super(type(self), self).__init__(*args, **kwargs)
     self.customer_id.choices = get_customer_list()
     self.courier_id.choices = get_courier_list()
     self.transaction_medium_id.choices = get_medium_list()
@@ -385,25 +385,25 @@ class FilterTableForm(Form):
   month = StringField(label='Month', validators=[Optional()])
   day = StringField(label='Day', validators=[Optional()])
   submit_button = SubmitField(label='Filter')
-  
+
   def validate(self, **kwargs):
-    
-    if not super().validate():
-      self.id.errors += (super().errors, 'super not validated')
+
+    if not super(type(self), self).validate():
+      self.id.errors += (super(type(self), self).errors, 'super not validated')
       return False
-    
+
     if self.year.data and all(x > 9999 or x < 0 for x in utils.map_csv_params(self.year.data, int)):
       self.year.errors += ('Year is not valid', 'Check your input format')
       return False
-    
+
     if self.month.data and all(x > 12 or x < 1 for x in utils.map_csv_params(self.month.data, int)):
       self.month.errors += ('Month is not valid', 'Check your input format')
       return False
-    
+
     if self.day.data and all(x > 31 or x < 1 for x in utils.map_csv_params(self.day.data, int)):
       self.day.errors += ('Day is not valid', 'Check your input format')
       return False
-    
+
     return True
 
 
@@ -411,12 +411,12 @@ class FilterTableForm(Form):
 class PerPageForm(Form):
   per_page = SelectField(label='Display Per Page',
                          choices=[(x, x) for x in config.PER_PAGE_DEFAULTS])
-  
+
   def __init__(self, per_page, *args, **kwargs):
     # unlike choices=..., default
     # cannot select default value by assigning self.per_page.default=...
     # alternative is https://groups.google.com/forum/#!topic/wtforms/6c656YspjRY
     if per_page in config.PER_PAGE_DEFAULTS:
       kwargs.setdefault('per_page', per_page)
-    
-    super().__init__(*args, **kwargs)  # set per page
+
+    super(type(self), self).__init__(*args, **kwargs)  # set per page
