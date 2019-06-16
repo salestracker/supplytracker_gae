8ca4be88a4583fea27dd8439186e17200718a2d1
codecakes <akurnya@gmail.com>
2019-06-16 19:14:42 +0530

    lots of changes to make docker build run

--- app/entry.py
+++
@@ -1,4 +0,0 @@
-from app import views
-from app import models
-from app import jinja_custom_filter
-from app.__init__ import app, db, babel
--- run.py
+++
@@ -1,8 +0,0 @@
-#!flask/bin/python
-from app.entry import app
-import os
-port = int(os.environ['PORT'])
-print('hello world!', port)
-
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=port, debug=True)
